518d7bf541164e2271a80709e1a0ef806ffc6322
o <o@office.com>
2017-02-15 08:58:06 +0800

    activate quick help in company

--- init/init-company.el
+++ init/init-company.el
@@ -23,7 +23,7 @@
   (with-eval-after-load 'company
     (setq
      ;;company-tooltip-limit 20 ; bigger popup window
-     company-idle-delay .3;菜单延迟
+     company-idle-delay .2;菜单延迟
      company-minimum-prefix-length 1; 开始补全字数
      company-echo-delay 0 ; remove annoying blinking
      company-begin-commands '(self-insert-command) ; start autocompletion only after typing
@@ -40,8 +40,8 @@
     (define-key company-active-map (kbd "C-p") 'company-select-previous)
     (global-set-key (kbd "<C-tab>") 'company-complete);手工补全热键
     
-    (with-eval-after-load 'company-quickhelp
-      (add-hook 'after-init-hook 'company-quickhelp-mode))
+    (company-quickhelp-mode 1)
+    (setq company-quickhelp-delay 0.2)
     )
 
 (provide 'init-company)
