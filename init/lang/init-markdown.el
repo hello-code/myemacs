87287d791438021dfac363f1aa99a42c5e46163f
u <u@u.com>
2017-09-30 16:34:04 +0800

    markdown mode config

--- init/lang/init-markdown.el
+++ init/lang/init-markdown.el
@@ -13,6 +13,14 @@
     (package-refresh-contents)
     (package-install package)))
 
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode"
+  "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
 
 (provide 'init-markdown)
 ;;; init-markdown ends here
