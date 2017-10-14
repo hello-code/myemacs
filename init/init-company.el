c3ecb38e8e846dc4547c365dd5b767a532689669
z <d@debian.com>
2017-10-14 11:59:38 +0800

    company

--- init/init-company.el
+++ init/init-company.el
@@ -19,30 +19,33 @@
   (unless (package-installed-p package)
     (package-install package)))
 
-  (add-hook 'after-init-hook 'global-company-mode)
-  (with-eval-after-load 'company
-    (setq
-     ;;company-tooltip-limit 20 ; bigger popup window
-     company-idle-delay .2;菜单延迟
-     company-minimum-prefix-length 1; 开始补全字数
-     company-echo-delay 0 ; remove annoying blinking
-     company-begin-commands '(self-insert-command) ; start autocompletion only after typing
-     company-require-match nil
-     ;;company-dabbrev-ignore-case nil
-     ;;company-dabbrev-downcase nil
-     company-show-numbers t; 显示序号
-     company-transformers '(company-sort-by-backend-importance)
-     company-continue-commands '(not helm-dabbrev)
-     )
+(add-hook 'after-init-hook 'global-company-mode)
+(with-eval-after-load 'company
+  (setq
+   ;;company-tooltip-limit 20 ; bigger popup window
+   company-idle-delay .2;菜单延迟
+   company-minimum-prefix-length 1; 开始补全字数
+   company-echo-delay 0 ; remove annoying blinking
+   company-begin-commands '(self-insert-command) ; start autocompletion only after typing
+   company-require-match nil
+   ;;company-dabbrev-ignore-case nil
+   ;;company-dabbrev-downcase nil
+   company-show-numbers t; 显示序号
+   company-transformers '(company-sort-by-backend-importance)
+   company-continue-commands '(not helm-dabbrev)
+   )
 
-    (define-key company-mode-map (kbd "<C-tab>") 'company-complete)
-    (define-key company-active-map (kbd "C-n") 'company-select-next)
-    (define-key company-active-map (kbd "C-p") 'company-select-previous)
-    (global-set-key (kbd "<C-tab>") 'company-complete);手工补全热键
-    
-    (company-quickhelp-mode 1)
-    (setq company-quickhelp-delay 0.2)
-    )
+  (define-key company-mode-map (kbd "<C-tab>") 'company-complete)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (global-set-key (kbd "<C-tab>") 'company-complete);手工补全热键
+  
+  (company-quickhelp-mode 1)
+  (setq company-quickhelp-delay 0.2)
+  )
+
+;; disable company mode in shell mode
+(setq company-global-modes '(not shell-mode))
 
 (provide 'init-company)
 ;;; init-company ends here
