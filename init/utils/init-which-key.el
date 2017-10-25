7fde2a1cc46999b76a63cd76dde1b719ff1e7e0d
d <d@mail>
2017-10-25 17:38:04 +0800

    config which-key

--- init/utils/init-which-key.el
+++ init/utils/init-which-key.el
@@ -1,5 +1,6 @@
 ;;; init-which-key.el --- which-key
 ;;; Commentary:
+;;; https://github.com/justbur/emacs-which-key#initial-setup
 ;;; Code:
 
 (require 'package)
@@ -13,15 +14,20 @@
 (which-key-mode)
 (which-key-setup-side-window-bottom)
 (setq which-key-idle-delay 0.1)
+(setq which-key-max-description-length 150)
+(setq which-key-prefix-prefix "+ ")
+(setq which-key-show-remaining-keys t)
 
 ;; 此处应与init-evil.el中的evil-leader对应
 (which-key-add-key-based-replacements
   "SPC j" "jump to..."
   "SPC h" "highlight symble"
   "SPC x" "xxx"
-  "C-c !" "flycheck"
   "SPC w" "ace window"
   "SPC y" "yasnippet"
+  "C-c !" "flycheck"
+  "C-c C-a" "markdown-insert"
+  "C-c C-c" "markdown"
   )
 
 (provide 'init-which-key)
