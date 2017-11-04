45d1c5274d8d72273065fe25be10e24178bf45d8
d <d@mail>
2017-11-04 17:14:54 +0800

    config python

--- init/lang/init-python.el
+++ init/lang/init-python.el
@@ -19,17 +19,23 @@
   (unless (package-installed-p package)
     (package-install package)))
 
-(eval-after-load "company"
-  '(add-to-list 'company-backends 'company-anaconda))
+;; python mode hook
+(defun my-python-mode-hook()
+  (exec-path-from-shell-copy-envs '("WORKON_HOME" "VIRTUALENV_PYTHON"))
+  (local-set-key (kbd "<f12>") 'anaconda-mode-find-definitions)
+  (local-set-key (kbd "M-/") 'anaconda-mode-go-back)
+  )
 
+(add-hook 'python-mode-hook 'my-python-mode-hook)
 (add-hook 'python-mode-hook 'anaconda-mode)
 (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
 
-;; virtual environment
-(require 'virtualenvwrapper)
-(venv-initialize-interactive-shells)
-(venv-initialize-eshell)
-(setq venv-location "~/development/python")
+(with-eval-after-load 'python-mode
+  (with-eval-after-load 'company
+    (add-hook 'python-mode-hook
+              '(lambda()
+                 (set(make-local-variable 'company-backends) '(company-anaconda))
+                 (company-mode)))))
 
 (with-eval-after-load 'python
   (defun python-shell-completion-native-try ()
@@ -41,8 +47,11 @@
        (get-buffer-process (current-buffer))
        nil "_"))))
 
-;; use .bashrc environment variable
-(exec-path-from-shell-copy-envs '("WORKON_HOME" "VIRTUALENV_PYTHON"))
+;; virtual environment
+(require 'virtualenvwrapper)
+(venv-initialize-interactive-shells)
+(venv-initialize-eshell)
+(setq venv-location "~/development/python")
 
 (provide 'init-python)
 ;;; init-python.el ends here
