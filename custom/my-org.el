6a16d1d338ea95b60d89b5d17511819f1bacd05d
z <z@debian>
2017-12-13 22:12:48 +0800

    new org config

--- custom/my-org.el
+++ custom/my-org.el
@@ -2,11 +2,54 @@
 ;;; Commentary:
 ;;; Code:
 
-(global-set-key "\C-ca" 'org-agenda)
-(setq org-agenda-files (quote ("~/git/org")))
+(require 'org)
+;; Agenda setup
+(setq org-agenda-files (list "~/org/local/"
+                             "~/org/sync/"
+                             ))
 
-;; 只在org模式中绑定该快捷键
+(global-set-key (kbd "<f11>") 'org-capture)
+(global-set-key (kbd "<f12>") 'org-agenda)
 
+;; TODO keyword
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING"))))
+
+(setq org-todo-keyword-faces
+      (quote (("TODO" :foreground "red" :weight bold)
+              ("NEXT" :foreground "#4B82F0" :weight bold)
+              ("DONE" :foreground "forest green" :weight bold)
+              ("WAITING" :foreground "orange" :weight bold)
+              ("HOLD" :foreground "magenta" :weight bold)
+              ("CANCELLED" :foreground "forest green" :weight bold)
+              ("MEETING" :foreground "forest green" :weight bold)
+              ("PHONE" :foreground "forest green" :weight bold))))
+
+;; fast TODO selection
+(setq org-use-fast-tag-selection t)
+;; allows changing TODO states with S-left/right
+(setq org-treat-S-cursor-todo-selection-as-state-change nil)
+
+;; Capture templates
+(require 'org-capture)
+(setq org-capture-templates
+      (quote (("t" "todo" entry (file "~/org/refile.org")
+               "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("r" "respond" entry (file "~/org/refile.org")
+               "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
+              ("n" "note" entry (file "~/org/refile.org")
+               "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("j" "Journal" entry (file+datetree "~/org/diary.org")
+               "* %?\n%U\n" :clock-in t :clock-resume t)
+              ("w" "org-protocol" entry (file "~/org/refile.org")
+               "* TODO Review %c\n%U\n" :immediate-finish t)
+              ("m" "Meeting" entry (file "~/org/refile.org")
+               "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
+              ("p" "Phone call" entry (file "~/org/refile.org")
+               "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
+              ("h" "Habit" entry (file "~/org/refile.org")
+               "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
 
 (provide 'my-org)
 ;;; my-org ends here
