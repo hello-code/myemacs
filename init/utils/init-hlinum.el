29649745535ddf8f3029239ef6e0eadef6d34c53
d <d@debian>
2018-03-28 17:13:51 +0800

    highlight row number color

--- init/utils/init-hlinum.el
+++ init/utils/init-hlinum.el
@@ -4,8 +4,13 @@
 
 (maybe-require-package 'hlinum)
 
-;(require 'hlinum)
+(require 'hlinum)
 (hlinum-activate)
+
+(with-eval-after-load 'hlinum
+  (setq linum-highlight-in-all-buffersp t)
+  (custom-set-faces
+   '(linum-highlight-face ((t (:background "#37474f" :foreground "orange" :weight bold))))))
 
 (provide 'init-hlinum)
 ;;; init-hlinum.el ends here
