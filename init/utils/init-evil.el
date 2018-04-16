6e1eea9800b7fc7d6fd16b1fe6b534ddfbaf97e8
d <d@debian>
2018-04-16 14:45:49 +0800

    add evil-org

--- init/utils/init-evil.el
+++ init/utils/init-evil.el
@@ -9,6 +9,7 @@
     evil-leader
     evil
     ace-jump-mode
+    evil-org
     ))
 
 (my-install-packages evil-packages)
@@ -44,6 +45,13 @@
    "w" 'ace-window
    )
   )
+
+;; evil-org
+(require 'evil-org)
+(add-hook 'org-mode-hook 'evil-org-mode)
+(evil-org-set-key-theme '(navigation insert textobjects additional calendar))
+(require 'evil-org-agenda)
+(evil-org-agenda-set-keys)
 
 (provide 'init-evil)
 ;;; init-evil ends here
