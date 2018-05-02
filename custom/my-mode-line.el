d09d1d29c6c89df0679f94bf23cea09975e12dca
z <z@d.com>
2018-05-02 22:50:09 +0800

    my-mode-line color

--- custom/my-mode-line.el
+++ custom/my-mode-line.el
@@ -3,19 +3,18 @@
 ;;; Code:
 
 (setq-default mode-line-position
-	      '((-3 "%p")(size-indication-mode("("(-4 "%I")")"))
-		" "
-		(line-number-mode ("%l" (column-number-mode ":%c")))
-		))
+              '((-3 "%p")(size-indication-mode("("(-4 "%I")")"))
+                " "
+                (line-number-mode ("%l" (column-number-mode ":%c")))
+                ))
 
-(set-face-attribute 'mode-line nil
-		    :foreground "white"
-		    :background "black"
-		    ;;:box '(:line-width 1 :color "#A1B57C"))
-		    :box '(:line-width 1 :color "#4B82F0" :style nil))
-(set-face-attribute 'mode-line-inactive nil
-		    :foreground "gray80" :background "gray40"
-		    :box '(:line-width 1 :color "#A1B57C" :style nil))
+(custom-set-faces
+ '(mode-line
+   ((t (:background "#1c1f26" :foreground "#ffffff" :box (:line-width 1 :color "#4b82f0")))))
+ '(mode-line-inactive
+   ((t ( :foreground "gray80" :background "gray40" :box '(:line-width 1 :color "#A1B57C" :style nil)))))
+ )
+
 
 (provide 'my-mode-line)
 ;;; my-mode-line ends here
