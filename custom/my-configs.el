a8cb9021c3573e5f7eb01cdb1c3a518b46829224
u <u@u.com>
2018-08-09 15:39:33 +0800

    face-font-recale-alist conflict with tooltip

--- custom/my-configs.el
+++ custom/my-configs.el
@@ -135,20 +135,20 @@
 ;;                             :underline nil :slant normal :weight bold
 ;;                             :family "DejaVu Sans Mono")))))
 
-;; ;; fonts (confilct with tooltip(company-quickhelp))
-;; (cond
-;;  ((eq system-type 'windows-nt)
-;;   (set-frame-font "Consolas-11")
-;;   (set-fontset-font "fontset-default" 'unicode "Microsoft Yahei 12")
-;;   (add-to-list 'default-frame-alist '(font . "Consolas-11"))
-;;   )
-;;  ((eq system-type 'gnu/linux)
-;;   (set-frame-font "DejaVu Sans Mono-11") ;; 10.5
-;;   (set-fontset-font "fontset-default" 'unicode "Noto Sans CJK SC")
-;;   ;;(set-fontset-font "fontset-default" 'unicode "WenQuanYi Bitmap Song 11")
-;;   (setq face-font-rescale-alist '(("Noto Sans CJK SC" . 1.18)))
-;;   (add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-11"))
-;;   ))
+;; fonts 
+(cond
+ ((eq system-type 'windows-nt)
+  (set-frame-font "Consolas-11")
+  (set-fontset-font "fontset-default" 'unicode "Microsoft Yahei 12")
+  (add-to-list 'default-frame-alist '(font . "Consolas-11"))
+  )
+ ((eq system-type 'gnu/linux)
+  (set-frame-font "DejaVu Sans Mono-11") ;; 10.5
+  (set-fontset-font "fontset-default" 'unicode "Noto Sans CJK SC")
+  ;;(set-fontset-font "fontset-default" 'unicode "WenQuanYi Bitmap Song 11")
+  ;;(setq face-font-rescale-alist '(("Noto Sans CJK SC" . 1.18))) ;; confilct with tooltip(company-quickhelp)
+  (add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-11"))
+  ))
 
 ;; mouse wheel font size
 (cond
