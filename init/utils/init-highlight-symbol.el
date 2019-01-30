40feee6ff959837fab5a52837eb43b213df31e17
win <w@win.com>
2019-01-30 17:26:39 +0800

    highlight symbol

--- init/utils/init-highlight-symbol.el
+++ init/utils/init-highlight-symbol.el
@@ -5,16 +5,17 @@
 ;;; Code:
 
 (use-package highlight-symbol
-  :defer t
+  ;;:defer t
   :ensure t
   :diminish(highlight-symbol-mode . "hls")
   :config
   (add-hook 'prog-mode-hook 'highlight-symbol-mode)
   (setq highlight-symbol-idle-delay 0.2)
   (setq highlight-symbol-highlight-single-occurrence nil)
-  (setq highlight-symbol-foreground-color "black")
+  ;;(setq highlight-symbol-foreground-color "black")
   :custom-face
-  (highlight-symbol-face ((t (:background "gray30" :inverse-video t))))
+  ;;(highlight-symbol-face ((t (:box(:line-width 1 :color "goldenrod")))))
+  (highlight-symbol-face ((t (:inherit default :background "dim gray"))))
   )
 
 (provide 'init-highlight-symbol)
