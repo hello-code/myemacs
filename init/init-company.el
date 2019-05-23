6b8efc864c012cc2d567aa1438f7670b7be9e71a
win <w@win.com>
2019-05-23 17:49:02 +0800

    company

--- init/init-company.el
+++ init/init-company.el
@@ -6,7 +6,17 @@
   ;;:defer t
   :ensure t
   :diminish (company-mode . "CMP")
-  :init  (add-hook 'after-init-hook 'global-company-mode)
+  :bind (("M-/" . company-complete)
+         ("<backtab>" . company-select-previous)
+         :map company-active-map
+         ("C-p" . company-select-previous)
+         ("C-n" . company-select-next)
+         ("<tab>" . company-complete-common-or-cycle)
+         ("<backtab>" . company-select-previous)
+         :map company-search-map
+         ("C-p" . company-select-previous)
+         ("C-n" . company-select-next))
+  :hook (after-init . global-company-mode)
   :config  
   (setq company-idle-delay 0.01
         company-show-numbers t
@@ -17,34 +27,28 @@
         company-selection-changed t
         company-tooltip-flip-when-above nil
         company-require-match nil
+        company-dabbrev-ignore-case nil
+        company-dabbrev-downcase nil
         pos-tip-border-width 1
 	    company-global-modes '(not shell-mode eshell-mode)
         company-transformers '(company-sort-by-occurrence) ; weight by frequency
         )
-  (define-key company-active-map (kbd "M-n") nil)
-  (define-key company-active-map (kbd "M-p") nil)
-  (define-key company-active-map (kbd "C-n") 'company-select-next)
-  (define-key company-active-map (kbd "C-p") 'company-select-previous)
-  (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
-  (define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)
-  (define-key company-active-map (kbd "S-TAB") 'company-select-previous)
-  (define-key company-active-map (kbd "<backtab>") 'company-select-previous)
   :custom-face
   (company-tooltip ((t (:background "#3c3c3c" :foreground "white"))))
   )
 
 (use-package company-quickhelp
-  ;; :ensure t
-  ;; :config
-  ;; (setq  company-quickhelp-max-lines 60
-  ;;        company-quickhelp-delay 0.2
-  ;;        company-quickhelp-color-background "#3c3c3c"
-  ;;        company-quickhelp-color-foreground "white")
-  ;; (define-key company-active-map (kbd "M-h") #'company-quickhelp-manual-begin)
-
-  :after company
+  :ensure t
+  ;; :defines company-quickhelp-delay
+  :after (company)
   :bind (:map company-active-map
-              ("C-c ?" . company-quickhelp-manual-begin)))
-
+              ("M-." . company-quickhelp-manual-begin))
+  ;; :hook (global-company-mode . company-quickhelp-mode)
+  :init
+  (setq  company-quickhelp-max-lines 60
+         company-quickhelp-delay 0.2
+         company-quickhelp-color-background "#3c3c3c"
+         company-quickhelp-color-foreground "white")
+  )
 (provide 'init-company)
 ;;; init-company.el ends here
